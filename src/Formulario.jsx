124cac72fd07f9dfad101eb3f5b16cc16b915b17
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-10-02 22:50:48 -0300

    agregue readme

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -3,45 +3,88 @@ import { Form, Button, Col, Row, Container } from "react-bootstrap";
 import ListaVete from "./ListaVete";
 
 const Formulario = () => {
+  const guardarLocalStorage =
+    JSON.parse(localStorage.getItem("listaMascota")) || [];
 
-    const guardarLocalStorage = JSON.parse(localStorage.getItem("listaMascota")) || [];
+  const [nombreMascota, setNombreMascota] = useState("");
+  const [nombreDuenio, setNombreDuenio] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [hora, setHora] = useState("");
+  const [sintomas, setSintomas] = useState("");
+  const [listaCita, setListaCita] = useState(guardarLocalStorage);
 
-    const [nombreMascota, setNombreMascota] = useState('');
-    const [nombreDuenio, setNombreDuenio] = useState('');
-    const [fecha, setFecha] = useState('');
-    const [hora, setHora] = useState('');
-    const [sintomas, setSintomas] = useState('');
-    const [listaCita, setListaCita] = useState(guardarLocalStorage);
+  useEffect(() => {
+    localStorage.setItem("listaMascota", JSON.stringify(listaCita));
+  }, [listaCita]);
 
-    useEffect(() => {
-      localStorage.setItem("listaMascota", JSON.stringify(listaCita));
-    }, [listaCita]);
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let objetoArreglos = { nombreMascota, nombreDuenio, fecha, hora, sintomas };
+    setListaCita([...listaCita, objetoArreglos]);
+    setNombreMascota("");
+    setNombreDuenio("");
+    setFecha("");
+    setHora("");
+    setSintomas("");
+  };
 
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        let objetoArreglos = {nombreMascota, nombreDuenio, fecha, hora, sintomas};
-        setListaCita([...listaCita, objetoArreglos]);
-        setNombreMascota("");
-        setNombreDuenio("");
-        setFecha("");
-        setHora("");
-        setSintomas("");
+  const borrarCard = (cita) => {
+    let arregloCitas = listaCita.filter((item) => item !== cita);
+    setListaCita(arregloCitas);
+  };
+
+  let mensaje = "";
+
+  if (listaCita.length === 0) {
+    mensaje = "No hay citas programadas";
+  } else {
+    mensaje = "";
+  }
+
+  const validacionMascota = (inputMascota) => {
+    if (nombreMascota.length < 2) {
+      inputMascota.classList.add("is-invalid");
+    } else {
+      inputMascota.classList.remove("is-invalid");
+      inputMascota.classList.add("is-valid");
     }
+  };
 
-    const borrarCard = (cita)=>{
-        let arregloCitas = listaCita.filter((item)=> item !== cita);
-        setListaCita(arregloCitas);
+  const validacionDuenio = (inputDuenio) => {
+    if (nombreDuenio.length < 7) {
+      inputDuenio.classList.add("is-invalid");
+    } else {
+      inputDuenio.classList.remove("is-invalid");
+      inputDuenio.classList.add("is-valid");
     }
-
-    let mensaje = "";
-    
-    if(listaCita.length === 0){
-        mensaje="No hay citas programadas"
-    }else{
-        mensaje="";
+  };
+  //VALIDAR
+  const validacionFecha = (inputFecho) => {
+    if (fecha.length < 2) {
+      inputFecho.classList.add("is-invalid");
+    } else {
+      inputFecho.classList.remove("is-invalid");
+      inputFecho.classList.add("is-valid");
     }
+  };
+  //VALIDAR
+  const validacionHora = (inputHora) => {
+    if (hora.length < 2) {
+      inputHora.classList.add("is-invalid");
+    } else {
+      inputHora.classList.remove("is-invalid");
+      inputHora.classList.add("is-valid");
+    }
+  };
 
+  const validacionSintoma = (inputSintoma) => {
+    if (sintomas.length < 7) {
+      inputSintoma.classList.add("is-invalid");
+    } else {
+      inputSintoma.classList.remove("is-invalid");
+      inputSintoma.classList.add("is-valid");
+    }
+  };
 
   return (
     <>
@@ -61,8 +104,12 @@ const Formulario = () => {
                 <Form.Control
                   type="text"
                   placeholder="Ingrese nombre de su mascota"
-                  onChange={(e)=> setNombreMascota(e.target.value)}
+                  onChange={(e) => {
+                    setNombreMascota(e.target.value);
+                    validacionMascota(e.target);
+                  }}
                   value={nombreMascota}
+                  required
                 />
               </Col>
             </Form.Group>
@@ -78,8 +125,12 @@ const Formulario = () => {
                 <Form.Control
                   type="text"
                   placeholder="Ingrese nombre del dueño"
-                  onChange={(e)=> setNombreDuenio(e.target.value)}
+                  onChange={(e) => {
+                    setNombreDuenio(e.target.value);
+                    validacionDuenio(e.target);
+                  }}
                   value={nombreDuenio}
+                  required
                 />
               </Col>
             </Form.Group>
@@ -89,9 +140,16 @@ const Formulario = () => {
                   Fecha
                 </Form.Label>
                 <Col md={8}>
-                  <Form.Control type="date" placeholder=""
-                  onChange={(e)=> setFecha(e.target.value)} 
-                  value={fecha}/>
+                  <Form.Control
+                    type="date"
+                    placeholder=""
+                    onChange={(e) => {
+                      setFecha(e.target.value);
+                      validacionFecha(e.target);
+                    }}
+                    value={fecha}
+                    required
+                  />
                 </Col>
               </Form.Group>
             </Col>
@@ -101,9 +159,16 @@ const Formulario = () => {
                   Hora
                 </Form.Label>
                 <Col md={8}>
-                  <Form.Control type="time" placeholder="" 
-                  onChange={(e)=> setHora(e.target.value)}
-                  value={hora}/>
+                  <Form.Control
+                    type="time"
+                    placeholder=""
+                    onChange={(e) => {
+                      setHora(e.target.value);
+                      validacionHora(e.target);
+                    }}
+                    value={hora}
+                    required
+                  />
                 </Col>
               </Form.Group>
             </Col>
@@ -112,9 +177,16 @@ const Formulario = () => {
                 Sintoma
               </Form.Label>
               <Col sm={10}>
-                <Form.Control type="text" placeholder="Ingrese los sintomas" 
-                onChange={(e)=> setSintomas(e.target.value)}
-                value={sintomas}/>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese los sintomas"
+                  onChange={(e) => {
+                    setSintomas(e.target.value);
+                    validacionSintoma(e.target);
+                  }}
+                  value={sintomas}
+                  required
+                />
               </Col>
             </Form.Group>
           </Row>
